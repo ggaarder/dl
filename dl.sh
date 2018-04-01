743c4a83dd91ab6605f2ce3f749655769c83658a
George Gaarder <ggaarder@foxmail.com>
2018-04-01 09:29:23 +0800

    add user agent since arXiv give out 403 if no

--- dl.sh
+++ dl.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+HEADER="User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+(KHTML, like Gecko) Chrome/39.0.2171.99 Safari/537.36"
 cd ~/home/download/websites
-wget --no-check-certificate --no-clobber -r $1 # use -r to create pathes, like /home/ggaarder/home/download/websites/www.togaware.com/linux/survivor/Creating_New0.html
+
+# use -r to create pathes, like
+# /home/ggaarder/home/download/websites/www.togaware.com/linux/survivor/
+# Creating_New0.html
+wget --no-check-certificate --no-clobber -r $1 --header="$HEADER"
